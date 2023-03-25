7321d71279d2b05c659e607ac417f2baf9645659
bia555555555 <104391703+bia555555555@users.noreply.github.com>
2023-03-25 17:37:33 -0300

    Update for fix errors

--- src/GraphQL/Entitys/graphql.entity.ts
+++ src/GraphQL/Entitys/graphql.entity.ts
@@ -2,17 +2,17 @@ import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 import { Field, Int, ObjectType } from '@nestjs/graphql';
 
 @ObjectType()
-@Entity()
+@Entity({ name: 'NestJSTable'})
 export class ExampleTable {
   @Field(() => Int)
-  @PrimaryGeneratedColumn()
-  id: number;
+  @PrimaryGeneratedColumn( {name: 'id'})
+  public id: number;
 
   @Field()
-  @Column()
-  name: string;
+  @Column({name: 'name'})
+  public name: string;
 
   @Field()
-  @Column()
-  email: string;
+  @Column({name: 'email'})
+  public email: string;
 }
--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,10 +1,5 @@
-import { GraphQLSchemaBuilderModule } from '@nestjs/graphql';
 import { Module } from '@nestjs/common';
 import { GraphQlModule } from '../GraphQl/modules/graphql.module'
-import { AppResolver } from '../GraphQL/Resolvers/graphql.resolver';
-
-
-
 
 @Module({
   imports: [GraphQlModule],
