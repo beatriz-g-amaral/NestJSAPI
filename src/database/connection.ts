8d025c07bb042152869eebf795cb727c3b613cca
bia555555555 <104391703+bia555555555@users.noreply.github.com>
2023-03-29 10:27:26 -0300

    Commenting TODO

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,8 +1,5 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
-import { ExampleTable } from '../entities/graphql.entity';
-import { Todo } from '../entities/todo.entity';
-import { TPT140 } from '../Company/entity/company.entity';
-import { TPT001 } from '../User/entity/user.entity';
+
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'oracle',
   host: process.env.DB_HOST,
@@ -14,12 +11,18 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
   entities: ["dist/**/*.entity.js"],
   synchronize: false,
 };
-// type: 'oracle',
-  // host: process.env.DB_HOST,
-  // port: 1521,
-  // username: process.env.DB_USERNAME,
-  // password: process.env.DB_PASSWORD,
-  // database: process.env.DB_DATABASE,
-  // sid: process.env.DB_SID,
-  // entities: [Todo, ExampleTable,TPT140,TPT001],
-  // synchronize: true,
+
+// TODO: Ver se realmente algo aqui pode ser aproveitado e se nao precisar capar fora
+// import { ExampleTable } from '../entities/graphql.entity';
+// import { Todo } from '../entities/todo.entity';
+// import { TPT140 } from '../Company/entity/company.entity';
+// import { TPT001 } from '../User/entity/user.entity';
+// // type: 'oracle',
+//   // host: process.env.DB_HOST,
+//   // port: 1521,
+//   // username: process.env.DB_USERNAME,
+//   // password: process.env.DB_PASSWORD,
+//   // database: process.env.DB_DATABASE,
+//   // sid: process.env.DB_SID,
+//   // entities: [Todo, ExampleTable,TPT140,TPT001],
+//   // synchronize: true,
