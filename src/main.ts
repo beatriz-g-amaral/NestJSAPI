d27dbebd3506d844bfcce4f9647fbd54a44fde53
bia555555555 <104391703+bia555555555@users.noreply.github.com>
2023-03-28 21:33:14 -0300

    removing this code

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { typeOrmConfig } from './database/connection';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  try {
-    await TypeOrmModule.forRoot(typeOrmConfig);
-    console.log('Database connection established successfully');
-  } catch (error) {
-    console.log(`Error connecting to database: ${error.message}`);
-  }
-
   await app.listen(3000);
 }
 
