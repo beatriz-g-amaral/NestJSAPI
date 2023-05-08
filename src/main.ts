508494c931df4c028f600d37cf1964aafd920c52
Beatriz Amaral <104391703+beatriz-g-amaral@users.noreply.github.com>
2023-05-08 16:50:01 -0300

    allow me to use on another project

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app.module';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Enable CORS for requests from localhost
+  app.use(
+    cors({
+      origin: 'http://localhost:8081',
+    }),
+  );
+
   await app.listen(3000);
 }
-
 bootstrap();
