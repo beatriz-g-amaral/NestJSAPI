b7ebb2f65435df305c8a7e2e01000891cf27aed6
Beatriz Amaral <104391703+beatriz-g-amaral@users.noreply.github.com>
2023-05-31 16:51:41 -0300

    Refactor

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,8 @@ import { NestExpressApplication } from '@nestjs/platform-express/interfaces/nest
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-
-  // Configurar o limite de carga
   app.use(express.json({ limit: '10mb' }));
   app.use(express.urlencoded({ limit: '10mb', extended: true }));
-  // Enable CORS for requests from localhost
   app.use(
     cors({
       origin: 'http://localhost:8080',
